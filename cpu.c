857887aed654edf67931f695e8e1301e60faa5fe
Tang Piseth <tangpiseth43@gmail.com>
2024-08-11 15:15:13 +0930

    More simulations done

--- cpu.c
+++ cpu.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <sys/time.h>
 #include "common.h"
-
+#include <unistd.h>
 
 int main(int argc, char** argv){
     if (argc != 2){
@@ -13,7 +13,7 @@ int main(int argc, char** argv){
     char* str = argv[1];
     while (1){
         Spin(1);
-        printf("%s\n", str);
+        printf("PID = %d, str = %s\n", getpid(), str);
     }
     
     return 0;
