c15ff763659a08aee345d8677429a9621e09aa4e
Tang Piseth <tangpiseth43@gmail.com>
2024-08-14 23:58:10 +0930

    Takss

--- even.c
+++ even.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
+#include <string.h>
 
 // Handler for SIGHUP 
 void handle_sighup(int sig){
@@ -15,39 +14,43 @@ void handle_sigint(int sig){
     printf("Yeah!\n");
 }
 
-
-
-int main(int argc, char *argv[]){
-    printf("PID: %d", getpid());
-    int n = 0;
-    // if (argc < 2){
-    //     // printf("Please enter an argument.\n");
-    //     exit(1);
-    // }
-    // else{// argc >= 2 (although we want it to be exactly 2)
-        
-        // Register signal handlers 
-        signal(SIGHUP, handle_sigint); 
-        signal(SIGINT, handle_sigint);
-
-
-        n = atoi(argv[1]); // number of even numbers to print out 
-        int i = 0; // Count number of even numbers 
-        
-        for (int j = 0; i < n; j++){ // BEGIN LOOP 
-            if (j % 2 == 0){
-                printf("%d ", j);
-                sleep(5);
-                i++; // we have i even numbers so far in this iteration (Loop invariant correctness proof)
-            }
-            // else printf("not even ");
-        } // END LOOP  
-
-
-
+int main(int argc, char *argv[]) {
+    // if (argc < 2) {
+    //     printf("Usage: %s <number of even numbers>\n", argv[0]);
+    //     return 1;
     // }
 
+    int n = atoi(argv[1]); // Number of even numbers to print
+    // if (n <= 0) {
+    //     printf("Please provide a positive integer.\n");
+    //     return 1;
+    // }
+
+    // printf("PID: %d\n", getpid());
+
+    // Register signal handlers using sigaction
+    struct sigaction sa;
+    sa.sa_handler = handle_sighup;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGHUP, &sa, NULL);
+
+    sa.sa_handler = handle_sigint;
+    sigaction(SIGINT, &sa, NULL);
+
+    int count = 0;
+    for (int i = 0; count < n; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+            fflush(stdout);
+            sleep(5); // Sleep for 5 seconds
+
+            // Prompt for user to press Enter to continue
+            // printf("\nPress Enter to continue...");
+            getchar(); // Wait for Enter key
+            count++; // Increment count of even numbers printed
+        }
+    }
 
     return 0;
 }
-
