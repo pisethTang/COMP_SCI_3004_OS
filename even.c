aa78df900e83e5358f3b7871afe4f3dedd49b125
Tang Piseth <tangpiseth43@gmail.com>
2024-08-14 23:33:53 +0930

    Finished part 1

--- even.c
+++ even.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 #include <stdlib.h>
+#include <signal.h>
 
-int main(int argc, char* argv[]){
-    // Check for invalid input 
+// Handler for SIGHUP 
+void handle_sighup(int sig){
+    printf("Ouch!\n");
+}
+
+// Handler for SIGINT 
+void handle_sigint(int sig){
+    printf("Yeah!\n");
+}
+
+
+
+int main(int argc, char *argv[]){
+    printf("PID: %d", getpid());
+    int n = 0;
     if (argc < 2){
-        printf("Please enter a number.\n");
+        printf("Please enter an argument.\n");
         exit(1);
     }
-    // Takes the input n from the command line
-    int number = atoi(argv[1]); // use the atoi() function from the standard library stdlib.h to convert the string representation of a number to an integer
-    // printf("argv[0]: name of the program = %s\n", argv[0]);
-    // printf("Entered number: %d and %d\n", number, atoi(argv[2]));    
-    // Prints the first n even numbers 
-    for (int i=0; i<number; i++){
-        if (i % 2 == 0) 
-        {
-            printf("%d", i);
-            sleep(5);
-        }
-        // scanf("");
-        // printf("scanned %d\n", i);
-        // printf("bye %d", i);
+    else{// argc >= 2 (although we want it to be exactly 2)
+        
+        // Register signal handlers 
+        signal(SIGHUP, handle_sigint); 
+        signal(SIGINT, handle_sigint);
+
+
+        n = atoi(argv[1]); // number of even numbers to print out 
+        int i = 0; // Count number of even numbers 
+        
+        for (int j = 0; i < n; j++){ // BEGIN LOOP 
+            if (j % 2 == 0){
+                printf("%d ", j);
+                sleep(5);
+                i++; // we have i even numbers so far in this iteration (Loop invariant correctness proof)
+            }
+            // else printf("not even ");
+        } // END LOOP  
+
+
+
     }
-    printf("\n");
 
 
     return 0;
 }
+
