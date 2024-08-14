04139c6ab8a25d0c44c09541c70952d735885252
Tang Piseth <tangpiseth43@gmail.com>
2024-08-14 23:46:36 +0930

    Task 2

--- miniShell.c
+++ miniShell.c
@@ -20,7 +20,7 @@
 char line[NL]; /*Command input buffer*/
 
 void prompt(void){
-    fprintf(stdout, "\n msh> ");
+    fprintf(stdout, "\nmsh> ");
     fflush(stdout);
 }
 
@@ -34,6 +34,9 @@ int main(int argk, char* argv[], char* envp[]){
     char *v[NV];        /*Array of pointers to command line tokens*/
     char *sep = "\t\n"; /*Command line token separators*/ 
     int i;              /*Parse index*/
+    int bg;             /*Background flag*/
+
+
 
     /*Prompt for and process one command line at a time*/
     while(1){ /*Loop forever*/
@@ -56,103 +59,51 @@ int main(int argk, char* argv[], char* envp[]){
             if (v[i] == NULL) break;
         }
 
+        /* Check for background processes */
+        if (strcmp(v[i-1], "&") == 0){
+            bg = 1;
+            v[i-1] = NULL; /* Remove '&' from arguments*/
+        }
+        else{
+            bg = 0;
+        }
+
+        /* Handle built-in commands like cd*/
+        if (strcmp(v[0], "cd") == 0){
+            if (chdir(v[1]) != 0){
+                perror("cd failed");
+            }
+            continue; /* Skip the rest of the loop*/
+        }
+
         /*Assert is number of tokens + 1*/
+
         /*Fork a child process to exec the command in v[0]*/
 
-        switch(frkRtnVal == fork()){
-            case -1: break; /*Fork returns error to parent process*/
-            case 0: execvp(v[0], v); /*code executed only by the child process*/
+        switch(frkRtnVal = fork()){
+            case -1: 
+                perror("fork failed");
+                break; /*Fork returns error to parent process*/
+            case 0: 
+                execvp(v[0], v); /*code executed only by the child process*/
+                perror("exec failed");
+                exit(1);
+
             default: /* code executed only by the parent process*/
-            {       
-                wpid = wait(0);
-                printf("%s done \n", v[0]);
+                if (!bg) {
+                    wpid = wait(0);
+                    printf("%s done\n", v[0]);
+                } else {
+                    printf("[%d] %s running in background\n", frkRtnVal, v[0]);
+                }
                 break;
-            }
+            // {       
+            //     wpid = wait(0);
+            //     printf("%s done \n", v[0]);
+            //     break;
+            // }
         } /*END switch*/
     } /*END while*/
 
     return 0;
 }
-
-/*********************************************************************
-Program : miniShell Version : 1.3
---------------------------------------------------------------------
-skeleton code for linix/unix/minix command line interpreter
---------------------------------------------------------------------
-File : minishell.c
-Compiler/System : gcc/linux
-********************************************************************/
-// #include <sys/types.h>
-// #include <sys/wait.h>
-// #include <stdio.h>
-// #include <string.h>
-// #include <unistd.h>
-// #include <stdlib.h>
-// #include <signal.h>
-// #define NV 20 /* max number of command tokens */
-// #define NL 100 /* input buffer size */
-// char line[NL]; /* command input buffer */
-// /*
-// shell prompt
-// */
-// prompt(void)
-// {
-// fprintf(stdout, "\n msh> ");
-// fflush(stdout);
-// }
-// main(int argk, char *argv[], char *envp[])
-// /* argk - number of arguments */
-// /* argv - argument vector from command line */
-// /* envp - environment pointer */
-// {
-// int frkRtnVal; /* value returned by fork sys call */
-// int wpid; /* value returned by wait */
-// char *v[NV]; /* array of pointers to command line tokens */
-// char *sep = " \t\n";/* command line token separators */
-// int i; /* parse index */
-// /* prompt for and process one command line at a time */
-// while (1) { /* do Forever */
-// prompt();
-// fgets(line, NL, stdin);
-// fflush(stdin);
-// if (feof(stdin)) { /* non-zero on EOF */
-// fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
-// feof(stdin), ferror(stdin));
-// exit(0);
-// }
-// if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
-// continue; /* to prompt */
-// v[0] = strtok(line, sep);
-// for (i = 1; i < NV; i++) {
-// v[i] = strtok(NULL, sep);
-// if (v[i] == NULL)
-// break;
-// }
-// /* assert i is number of tokens + 1 */
-// /* fork a child process to exec the command in v[0] */
-// switch (frkRtnVal = fork()) {
-// case -1: /* fork returns error to parent process */
-// {
-// break;
-// }
-// case 0: /* code executed only by child process */
-// {
-// execvp(v[0], v);
-// }
-// default: /* code executed only by parent process */
-// {
-// wpid = wait(0);
-// printf("%s done \n", v[0]);
-// break;
-// }
-// } /* switch */
-// } /* while */
-// } /* main */
-
-
-
-
-
-
-
-// */
