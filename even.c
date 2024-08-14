8bdc0a1b09f5ddfdd38a72bbf6f3823797efe573
Tang Piseth <tangpiseth43@gmail.com>
2024-08-15 00:26:28 +0930

    Takss

--- even.c
+++ even.c
@@ -2,55 +2,55 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-#include <string.h>
 
 // Handler for SIGHUP 
-void handle_sighup(int sig){
+void handle_sighup(int sig) {
     printf("Ouch!\n");
+    // Re-register the signal handler
+    // signal(SIGHUP, handle_sighup);
 }
 
 // Handler for SIGINT 
-void handle_sigint(int sig){
+void handle_sigint(int sig) {
     printf("Yeah!\n");
+    // Re-register the signal handler
+    // signal(SIGINT, handle_sigint);
 }
 
 int main(int argc, char *argv[]) {
-    // if (argc < 2) {
-    //     printf("Usage: %s <number of even numbers>\n", argv[0]);
-    //     return 1;
-    // }
+    // Register signal handlers using sigaction
+    struct sigaction sa_hup, sa_int;
+    
+    sa_hup.sa_handler = handle_sighup;
+    sigemptyset(&sa_hup.sa_mask);
+    sa_hup.sa_flags = 0;
+    
+    sa_int.sa_handler = handle_sigint;
+    sigemptyset(&sa_int.sa_mask);
+    sa_int.sa_flags = 0;
+    
+    // Register signal handlers
+    sigaction(SIGHUP, &sa_hup, NULL);
+    sigaction(SIGINT, &sa_int, NULL);
 
-    int n = atoi(argv[1]); // Number of even numbers to print
-    // if (n <= 0) {
-    //     printf("Please provide a positive integer.\n");
-    //     return 1;
-    // }
-
+    // Print the process ID
     // printf("PID: %d\n", getpid());
 
-    // Register signal handlers using sigaction
-    struct sigaction sa;
-    sa.sa_handler = handle_sighup;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_flags = 0;
-    sigaction(SIGHUP, &sa, NULL);
+    // if (argc < 2) {
+    //     printf("Please enter an argument.\n");
+    //     return 1;
+    // }
 
-    sa.sa_handler = handle_sigint;
-    sigaction(SIGINT, &sa, NULL);
+    int n = atoi(argv[1]); // Number of even numbers to print out
+    int i = 0; // Count number of even numbers
 
-    int count = 0;
-    for (int i = 0; count < n; i++) {
-        if (i % 2 == 0) {
-            printf("%d ", i);
-            fflush(stdout);
-            sleep(5); // Sleep for 5 seconds
-
-            // Prompt for user to press Enter to continue
-            // printf("\nPress Enter to continue...");
-            getchar(); // Wait for Enter key
-            count++; // Increment count of even numbers printed
+    for (int j = 0; i < n; j++) { // BEGIN LOOP
+        if (j % 2 == 0) {
+            printf("%d\n", j);
+            sleep(5);
+            i++; // We have i even numbers so far in this iteration
         }
-    }
+    } // END LOOP
 
     return 0;
 }
--- miniShell.c
+++ miniShell.c
@@ -5,109 +5,44 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <signal.h>
-
-#define NV 20   /* max number of command tokens */
-#define NL 100  /* input buffer size */
-
+#define NV 20 /* max number of command tokens */
+#define NL 100 /* input buffer size */
 char line[NL]; /* command input buffer */
 
-typedef struct Job {
-    int pid;
-    int id;
-    char command[NL];
-    struct Job *next;
-} Job;
-
-Job *jobs = NULL;
-int job_count = 0;
-
-/*
-shell prompt
-*/
-void prompt(void) {
-    fprintf(stdout, "\nmsh> ");
-    fflush(stdout);
-}
-
-void add_job(int pid, char *command) {
-    Job *new_job = (Job *)malloc(sizeof(Job));
-    new_job->pid = pid;
-    new_job->id = ++job_count;
-    strcpy(new_job->command, command);
-    new_job->next = jobs;
-    jobs = new_job;
-    printf("[%d] %d\n", new_job->id, pid);
-}
-
-void check_jobs() {
-    Job *current = jobs;
-    Job *prev = NULL;
-
-    while (current != NULL) {
-        int status;
-        int result = waitpid(current->pid, &status, WNOHANG);
-
-        if (result == 0) {
-            prev = current;
-            current = current->next;
-        } else if (result == -1) {
-            perror("waitpid");
-            if (prev == NULL) {
-                jobs = current->next;
-            } else {
-                prev->next = current->next;
-            }
-            Job *temp = current;
-            current = current->next;
-            free(temp);
-        } else {
-            printf("[%d]+ Done %s\n", current->id, current->command);
-            if (prev == NULL) {
-                jobs = current->next;
-            } else {
-                prev->next = current->next;
-            }
-            Job *temp = current;
-            current = current->next;
-            free(temp);
-        }
-    }
-}
-
 int main(int argk, char *argv[], char *envp[]) {
-    int frkRtnVal;   /* value returned by fork sys call */
-    char *v[NV];     /* array of pointers to command line tokens */
+    int frkRtnVal; /* value returned by fork sys call */
+    int wpid; /* value returned by wait */
+    int status; /* status for wait */
+    char *v[NV]; /* array of pointers to command line tokens */
     char *sep = " \t\n"; /* command line token separators */
-    int i;           /* parse index */
+    int i; /* parse index */
+    int bg_processes = 0; /* count of background processes */
 
-    /* prompt for and process one command line at a time */
     while (1) { /* do Forever */
-        prompt();
-        fgets(line, NL, stdin);
-        fflush(stdin);
+        // Prompt
+        printf("\nmsh> ");
+        fflush(stdout);
 
-        if (feof(stdin)) { /* non-zero on EOF */
-            fprintf(stderr, "EOF pid %d feof %d ferror %d\n", getpid(),
-                    feof(stdin), ferror(stdin));
-            exit(0);
+        // Read command
+        if (fgets(line, NL, stdin) == NULL) {
+            if (feof(stdin)) {
+                exit(0);
+            }
+            perror("fgets");
+            continue;
         }
-        if (line[0] == '#' || line[0] == '\n' || line[0] == '\000')
-            continue; /* to prompt */
 
+        // Tokenize input
         v[0] = strtok(line, sep);
         for (i = 1; i < NV; i++) {
             v[i] = strtok(NULL, sep);
-            if (v[i] == NULL)
-                break;
+            if (v[i] == NULL) break;
         }
 
-        int background = 0;
-        if (i > 1 && strcmp(v[i - 1], "&") == 0) {
-            background = 1;
-            v[i - 1] = NULL;
-        }
+        // Handle empty command
+        if (v[0] == NULL) continue;
 
-        /* Handle the built-in "cd" command */
+        // Handle `cd` command
         if (strcmp(v[0], "cd") == 0) {
             if (v[1] == NULL || chdir(v[1]) != 0) {
                 perror("cd");
@@ -115,29 +50,44 @@ int main(int argk, char *argv[], char *envp[]) {
             continue;
         }
 
-        /* fork a child process to exec the command in v[0] */
-        switch (frkRtnVal = fork()) {
-        case -1: /* fork returns error to parent process */
-            perror("fork");
-            break;
-
-        case 0: /* code executed only by child process */
-            execvp(v[0], v);
-            perror("execvp"); // If execvp fails
-            _exit(EXIT_FAILURE);
-
-        default: /* code executed only by parent process */
-            if (background) {
-                add_job(frkRtnVal, v[0]);
-            } else {
-                if (waitpid(frkRtnVal, NULL, 0) == -1) {
-                    perror("waitpid");
-                }
-                printf("%s done\n", v[0]);
-            }
-            break;
+        // Check for background process request
+        int background = 0;
+        if (v[i-1] != NULL && strcmp(v[i-1], "&") == 0) {
+            background = 1;
+            v[i-1] = NULL; // Remove the "&" from the arguments
         }
 
-        check_jobs(); // Check if any background jobs have completed
+        // Fork a child process
+        frkRtnVal = fork();
+        if (frkRtnVal < 0) {
+            perror("fork");
+            continue;
+        } else if (frkRtnVal == 0) { // Child process
+            execvp(v[0], v);
+            // If execvp fails
+            perror("execvp");
+            exit(EXIT_FAILURE);
+        } else { // Parent process
+            if (background) {
+                bg_processes++;
+                printf("[%d] %d\n", bg_processes, frkRtnVal);
+            } else {
+                wpid = waitpid(frkRtnVal, &status, 0);
+                if (wpid == -1) {
+                    perror("waitpid");
+                } else {
+                    if (WIFEXITED(status)) {
+                        printf("%s done\n", v[0]);
+                    }
+                }
+            }
+
+            // Check if any background processes have finished
+            while ((wpid = waitpid(-1, &status, WNOHANG)) > 0) {
+                printf("[%d]+ Done %s\n", bg_processes, v[0]);
+                bg_processes--;
+            }
+        }
     }
+    return 0;
 }
