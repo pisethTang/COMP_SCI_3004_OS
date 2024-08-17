9e209a02f9a6b9ce315bb2d839a05404124cf12f
Tang Piseth <tangpiseth43@gmail.com>
2024-08-17 22:42:02 +0930

    Tasks

---
+++ CodeVault/mult_forks.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+
+
+int main(int argc, char** argv){
+    int id1 = fork();
+    int id2 = fork();   
+    // Now we have 4 processes. 
+    // But there is still only one parent process and the remaining 3 are children
+
+
+
+    if (id1 == 0){
+        if (id2 == 0) printf("We are process y.\n");
+        else printf("We are process x.\n");
+    }
+    else{
+        if (id2 == 0){
+            printf("We are process z.\n");
+        }
+        else printf("We are the parent process.\n");
+    }
+    /*
+    Expected: Should print out one of the above conditions. 
+    */
+
+
+
+    /*
+        Now, how do we process     
+    */
+
+
+
+   
+
+    return 0;
+}
