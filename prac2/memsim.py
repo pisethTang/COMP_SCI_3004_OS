ef88251aa04857e553b04cde60b495aa1c810bee
Tang Piseth <tangpiseth43@gmail.com>
2024-09-03 13:14:49 +0930

    Trying python scripts

--- prac2/memsim.py
+++ prac2/memsim.py
@@ -29,7 +29,7 @@ def main():
 
     frames = int(sys.argv[2])
     if frames < 1:
-       printf( "Frame number must be at least 1\n");
+       print( "Frame number must be at least 1\n");
        return
 
     replacement_mode = sys.argv[3]
@@ -39,7 +39,7 @@ def main():
         mmu = RandMMU(frames)
     elif replacement_mode == "lru":
         mmu = LruMMU(frames)
-    elif replacement_mode == "esc":
+    elif replacement_mode == "clock":
         mmu = ClockMMU(frames)
     else:
         print("Invalid replacement mode. Valid options are [rand, lru, esc]")
