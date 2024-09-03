e1325381770887e59239550a1fad27dec6c4ff1a
Tang Piseth <tangpiseth43@gmail.com>
2024-09-03 22:15:27 +0930

    Test GS

--- prac2/clockmmu.py
+++ prac2/clockmmu.py
@@ -3,8 +3,8 @@ from mmu import MMU
 class ClockMMU(MMU):
     def __init__(self, frames):
         self.frames = frames
-        self.page_table = {}  # {page_number: [frame_index, modified_bit, use_bit]}
-        self.frame_list = []  # [(page_number, modified_bit, use_bit)]
+        self.page_table = {}  # {page_number: [frame_index, modified_bit, reference_bit]}
+        self.frame_list = []  # [(page_number, modified_bit, reference_bit)]
         self.clock_hand = 0
         self.total_disk_reads = 0
         self.total_disk_writes = 0
@@ -21,49 +21,39 @@ class ClockMMU(MMU):
         if page_number not in self.page_table:
             self.handle_page_fault(page_number, False)
         else:
-            # Page is already in memory, just update the use bit
-            frame_index, modified_bit, use_bit = self.page_table[page_number]
-            self.page_table[page_number] = [frame_index, modified_bit, 1]  # Set use bit to 1
+            frame_index, modified_bit, reference_bit = self.page_table[page_number]
+            self.page_table[page_number] = [frame_index, modified_bit, 1]  # Set reference bit
 
     def write_memory(self, page_number):
         if page_number not in self.page_table:
             self.handle_page_fault(page_number, True)
         else:
-            # Page is already in memory, update both modified and use bits
-            frame_index, modified_bit, use_bit = self.page_table[page_number]
-            self.page_table[page_number] = [frame_index, 1, 1]  # Set modified and use bits to 1
+            frame_index, modified_bit, reference_bit = self.page_table[page_number]
+            self.page_table[page_number] = [frame_index, 1, 1]  # Set modified and reference bits
 
     def handle_page_fault(self, page_number, is_write):
         self.total_page_faults += 1
-        self.total_disk_reads += 1  # We read from disk only when there is a page fault
+        self.total_disk_reads += 1
 
         if len(self.frame_list) < self.frames:
-            # There is still space in the frame list, add new page directly
             frame_index = len(self.frame_list)
             self.page_table[page_number] = [frame_index, int(is_write), 1]
             self.frame_list.append((page_number, int(is_write), 1))
         else:
-            # No space left, need to find a page to replace using the Clock algorithm
             while True:
-                current_page, modified_bit, use_bit = self.frame_list[self.clock_hand]
-
-                if use_bit == 0:
-                    # Found a page to replace
-                    if modified_bit == 1:
-                        self.total_disk_writes += 1  # Only increment disk writes if the page was modified
-
-                    # Replace the current page with the new page
-                    self.page_table.pop(current_page)
-                    self.page_table[page_number] = [self.clock_hand, int(is_write), 1]
-                    self.frame_list[self.clock_hand] = (page_number, int(is_write), 1)
-
-                    # Advance the clock hand
-                    self.clock_hand = (self.clock_hand + 1) % self.frames
+                current_page, modified_bit, reference_bit = self.frame_list[self.clock_hand]
+                if reference_bit == 0:
                     break
-                else:
-                    # Reset the use bit and advance the clock hand
-                    self.frame_list[self.clock_hand] = (current_page, modified_bit, 0)
-                    self.clock_hand = (self.clock_hand + 1) % self.frames
+                self.frame_list[self.clock_hand] = (current_page, modified_bit, 0)
+                self.clock_hand = (self.clock_hand + 1) % self.frames
+
+            if modified_bit == 1:
+                self.total_disk_writes += 1
+
+            self.page_table.pop(current_page)
+            self.page_table[page_number] = [self.clock_hand, int(is_write), 1]
+            self.frame_list[self.clock_hand] = (page_number, int(is_write), 1)
+            self.clock_hand = (self.clock_hand + 1) % self.frames
 
     def get_total_disk_reads(self):
         return self.total_disk_reads
