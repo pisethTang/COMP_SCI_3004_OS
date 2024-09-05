84dbf5499d4545f54771b06dd6b95a8e96b18b2e
Tang Piseth <tangpiseth43@gmail.com>
2024-09-05 22:42:32 +0930

    Added

---
+++ Ch22-Concurrency/single_threaded.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+#include <pthread.h>
+
+
+
+void* my_thread(void* arg){
+  printf("%s\n", (char*)arg);
+  return NULL;
+}
+
+int main(int argc, char* argv[]){
+  pthread_t p1, p2;
+  int rc;
+  printf("main: begin\n");
+  rc = pthread_create(&p1, NULL, my_thread, "A"); assert(rc==0);
+  rc = pthread_create(&p2, NULL, my_thread, "B"); assert(rc==0);
+  printf("main: end\n");
+  
+
+  return 0;
+}
